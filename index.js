14ab7466b86a4bac6afa8135c76d2f8738336a28
Alan Gutierrez <alan@prettyrobots.com>
2013-12-25 21:52:07 -0800

    Sketch of merge.
    Closes #6.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-module.exports = 1
+var cadence = require('cadence')
+var ok = require('assert').ok
+
+function Amalgamator (deleted, primary, iterator) {
+    this._deleted = deleted
+    this._primary = primary
+    this._iterator = iterator
+}
+
+Amalgamator.prototype.amalgamate = cadence(function (step) {
+    var insert
+    step(function () {
+        this._iterator.next(step())
+    }, function (record, key) {
+        if (record && key) insert = step(function () {
+           key.version = record.version = 0
+           if (!this._mutator) {this._primary.mutator(key, step(step, function ($) {
+                this._mutator = $
+                return this._mutator.index
+            }))} else {
+                this._mutator.indexOf(key, step())
+            }
+        }, function (index) {
+            if (index < 0) return ~ index
+            else step(function () {
+                this._mutator.remove(index, step())
+            }, function () {
+                this._mutator.indexOf(key, step())
+            })
+        }, function (index) {
+            if (!this._deleted(record)) step(function () {
+                this._mutator.insert(record, key, index, step())
+            }, function (result) {
+                if (result != 0) {
+                    ok(result > 0, 'went backwards')
+                    this._mutator.unlock()
+                    delete this._mutator
+                    step(insert)
+                }
+            })
+        })(1)
+        else step(null)
+    })()
+})
+
+Amalgamator.prototype.unlock = function () {
+    if (this._mutator) this._mutator.unlock()
+}
+
+exports.amalgamate = cadence(function (step, deleted, primary, iterator) {
+    var amalgamator = new Amalgamator(deleted, primary, iterator)
+    step(function () {
+        amalgamator.amalgamate(step())
+    }, function () {
+        amalgamator.unlock()
+    })
+})
---
+++ t/amalgamate/amalgamate.t.js
@@ -0,0 +1,64 @@
+require('./proof')(2, function (step, serialize, deepEqual, Strata, tmp, gather) {
+    var amalgamate = require('../..')
+    var designate = require('designate')
+    var skip = require('skip')
+    var mvcc = require('mvcc')
+    var fs = require('fs')
+    function deleted () {
+        return false
+    }
+    function extractor (record) {
+        return record.value
+    }
+    function comparator (a, b) {
+        return a < b ? -1 : a > b ? 1 : 0
+    }
+    var names = 'primary one two three'.split(/\s+/)
+    step(function () {
+        names.forEach(step([], function (name) {
+            step(function () {
+                fs.mkdir(tmp + '/' + name, step())
+            }, function () {
+                serialize(__dirname + '/fixtures/' + name + '.json', tmp + '/' + name, step())
+            }, function () {
+                var strata = new Strata({
+                    extractor: mvcc.extractor(extractor),
+                    comparator: mvcc.comparator(comparator),
+                    leafSize: 3, branchSize: 3,
+                    directory: tmp + '/' + name
+                })
+                step(function () {
+                    strata.open(step())
+                }, function () {
+                    return strata
+                })
+            })
+        }))
+    }, function (stratas) {
+        var primary = stratas.shift()
+        step(function () {
+            stratas.forEach(step([], function (strata) {
+                skip.forward(strata, comparator, { 0: true }, 'a', step())
+            }))
+        }, function (iterators) {
+            designate.forward(comparator, deleted, iterators, step())
+        }, function (iterator) {
+            step(function () {
+                amalgamate.amalgamate(deleted, primary, iterator, step())
+            }, function () {
+                iterator.unlock()
+                gather(step, primary)
+            })
+        }, function (records) {
+            var versions
+            versions = records.map(function (record) { return record.version })
+            records = records.map(function (record) { return record.value })
+            deepEqual(records, [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' ], 'records')
+            deepEqual(versions, [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ], 'versions')
+        }, function () {
+            step(function (strata) {
+                strata.close(step())
+            })(stratas.concat(primary))
+        })
+    })
+})
