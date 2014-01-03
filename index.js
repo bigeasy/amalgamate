fc18ee9a68d12b7b80e07f9b1e5324ecd87741ea
Alan Gutierrez <alan@prettyrobots.com>
2014-01-03 10:15:28 -0800

    Extract Splice module.
    Closes #21.

--- index.js
+++ index.js
@@ -1,58 +1,18 @@
-var cadence = require('cadence')
-var ok = require('assert').ok
+var splice = require('splice')
+var twiddle = require('twiddle')
 
-function Amalgamator (deleted, version, primary, iterator) {
-    this._deleted = deleted
-    this._version = version
-    this._primary = primary
-    this._iterator = iterator
+module.exports = function (deleted, version, primary, iterator, callback) {
+    function revise (object) {
+        if (object) {
+            var revised = {}
+            for (var key in object) revised[key] = object[key]
+            revised.version = version
+            return revised
+        }
+    }
+    splice(function (incoming, existing) {
+        return deleted(incoming.record, incoming.key) ? 'delete' : 'insert'
+    }, primary, twiddle(iterator, function (record, key, callback) {
+        callback(null, revise(record), revise(key))
+    }), callback)
 }
-
-Amalgamator.prototype.amalgamate = cadence(function (step) {
-    var insert
-    step(function () {
-        this._iterator.next(step())
-    }, function (record, key) {
-        if (record && key) insert = step(function () {
-           key.version = record.version = this._version
-           if (!this._mutator) {this._primary.mutator(key, step(step, function ($) {
-                this._mutator = $
-                return this._mutator.index
-            }))} else {
-                this._mutator.indexOf(key, step())
-            }
-        }, function (index) {
-            if (index < 0) return ~ index
-            else step(function () {
-                this._mutator.remove(index, step())
-            }, function () {
-                this._mutator.indexOf(key, step())
-            })
-        }, function (index) {
-            if (!this._deleted(record)) step(function () {
-                this._mutator.insert(record, key, index, step())
-            }, function (result) {
-                if (result != 0) {
-                    ok(result > 0, 'went backwards')
-                    this._mutator.unlock()
-                    delete this._mutator
-                    step(insert)
-                }
-            })
-        })(1)
-        else step(null)
-    })()
-})
-
-Amalgamator.prototype.unlock = function () {
-    if (this._mutator) this._mutator.unlock()
-}
-
-module.exports = cadence(function (step, deleted, version, primary, iterator) {
-    var amalgamator = new Amalgamator(deleted, version, primary, iterator)
-    step([function () {
-        amalgamator.unlock()
-    }], function () {
-        amalgamator.amalgamate(step())
-    })
-})
--- release.md
+++ release.md
@@ -1,3 +1,9 @@
+### Extract Splice Module
+
+Extracted the record merge logic to a module named Splice. Also, created a
+module named Twiddle that is a decorator for an MVCC iterator. I'm using it to
+rewrite the record and key with a new version.
+
 ### Single Function Module
 
 Amalgamate is now a single function module. It returns just the `amalgamate`
@@ -10,6 +16,7 @@ Upgraded [Skip](https://github.com/bigeasy/skip) and
 
 ### Issue by Issue
 
+ * Extract Splice module. #21.
  * Upgrade Designate to 0.0.2. #23.
  * Upgrade Skip to 0.0.4. #22.
  * Module should contain only the one function. #20.
