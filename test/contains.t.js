a8346aceffb145e12d89280fb1a42cf09e1c8aed
Alan Gutierrez <alan@prettyrobots.com>
2020-10-29 06:21:44 -0500

    Remove dead test for dead code.

--- test/contains.t.js
+++
@@ -1,5 +0,0 @@
-require('proof')(2, okay => {
-    const contains = require('../contains')
-    okay(contains({ a: true, b: true }, { a: true }), 'contains')
-    okay(!contains({ a: true, b: true }, { c: true }), 'does not contain')
-})
